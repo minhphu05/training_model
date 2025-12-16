9614c714518df13fb0a8becd806fe13b29376002
Minh Phu <63292874+minhphu05@users.noreply.github.com>
2025-12-16 16:03:46 +0700

    Update train_bilstm.py

--- training/train_bilstm.py
+++ training/train_bilstm.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     ).to(device)
     
     loss_fn = nn.CrossEntropyLoss(ignore_index=-100)
-    optimizer = optim.Adam(model.parameters(), lr=1e-2) 
+    optimizer = optim.Adam(model.parameters(), lr=1e-4) 
 
     epoch = 0
     best_f1 = 0
