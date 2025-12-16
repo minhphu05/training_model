dbcaace6917171b6eea3a21860eedf6ec466cfab
Minh Phu <63292874+minhphu05@users.noreply.github.com>
2025-12-17 00:18:34 +0700

    Add files via upload

---
+++ model/XLMR_Large.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from transformers import AutoModel
+from torchcrf import CRF
+from transformers import AutoModelForMaskedLM, AutoTokenizer, BitsAndBytesConfig
+
+class XLMR_CRF(nn.Module):
+    def __init__(self, num_tags):
+        super().__init__()
+        # Thay đổi model từ phobert sang xlm-roberta-base
+        self.xlmroberta = AutoModel.from_pretrained("xlm-roberta-large")
+        self.dropout = nn.Dropout(0.3)
+        self.fc = nn.Linear(768, num_tags)
+        self.crf = CRF(num_tags, batch_first=True)
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.xlmroberta(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        x = self.dropout(outputs.last_hidden_state)
+        emissions = self.fc(x)
+
+        if labels is not None:
+            loss = -self.crf(
+                emissions,
+                labels,
+                mask=attention_mask.bool()
+            )
+            return loss
+        else:
+            return self.crf.decode(
+                emissions,
+                mask=attention_mask.bool()
+            )
