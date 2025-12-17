c3f4068f99305d4ae9097bb9b9315de3ef929c1e
Minh Phu <63292874+minhphu05@users.noreply.github.com>
2025-12-17 12:23:49 +0700

    Update XLM_R.py

--- model/XLM_R.py
+++ model/XLM_R.py
@@ -27,28 +27,42 @@ class XLMR_BiLSTM(nn.Module):
         
         self.fc = nn.Linear(hidden_size * 2, num_tags)
 
-    def forward(self, input_ids, attention_mask, lengths):
+    # def forward(self, input_ids, attention_mask, lengths):
+    #     outputs = self.xlmroberta(
+    #         input_ids=input_ids,
+    #         attention_mask=attention_mask
+    #     )
+    #     x = self.dropout(outputs.last_hidden_state)  # (batch, seq_len, 768)
+        
+    #     # Xử lý pack padded sequence để bỏ phần padding
+    #     packed_input = pack_padded_sequence(
+    #         x, 
+    #         lengths.cpu(),
+    #         batch_first=True,
+    #         enforce_sorted=False
+    #     )
+        
+    #     packed_output, _ = self.bilstm(packed_input)
+        
+    #     output, _ = pad_packed_sequence(
+    #         packed_output,
+    #         batch_first=True
+    #     )
+        
+    #     output = self.dropout(output)
+    #     logits = self.fc(output)  # (batch, seq_len, num_tags)
+    #     return logits
+    def forward(self, input_ids, attention_mask):
         outputs = self.xlmroberta(
             input_ids=input_ids,
             attention_mask=attention_mask
         )
         x = self.dropout(outputs.last_hidden_state)  # (batch, seq_len, 768)
-        
-        # Xử lý pack padded sequence để bỏ phần padding
-        packed_input = pack_padded_sequence(
-            x, 
-            lengths.cpu(),
-            batch_first=True,
-            enforce_sorted=False
-        )
-        
-        packed_output, _ = self.bilstm(packed_input)
-        
-        output, _ = pad_packed_sequence(
-            packed_output,
-            batch_first=True
-        )
-        
-        output = self.dropout(output)
-        logits = self.fc(output)  # (batch, seq_len, num_tags)
+    
+        # Đừng dùng pack_padded_sequence ở đây
+        lstm_out, _ = self.bilstm(x)  # (batch, seq_len, hidden_size*2)
+    
+        lstm_out = self.dropout(lstm_out)
+        logits = self.fc(lstm_out)  # (batch, seq_len, num_tags)
         return logits
+    
